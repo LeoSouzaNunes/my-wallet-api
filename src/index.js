92e31d7152b3145960e7313241727c1cb33f2fd5
Leonardo de Souza Nunes <contato.leonunez@gmail.com>
2022-01-31 23:03:55 -0300

    Add initial setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => console.log("Running at http://localhost:5000"));
